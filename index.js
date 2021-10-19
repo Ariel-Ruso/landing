92b1f918e2db0b0c5a873cc10433a3c5e5642bc6
Ariel-Ruso <ariruso@gmail.com>
2021-10-19 08:06:10 -0300

    sass

---
+++ index.js
@@ -0,0 +1,8 @@
+const imgMain = document.getElementById('imgMain');
+const icon = document.getElementById('icon');
+const h1 = document.getElementById('h1');
+
+icon.onclick = function() {
+    h1.style.color = "red";
+    imgMain.style.width = "100%";
+}
